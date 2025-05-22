e0e5845acdad0f5d09fff0c8cfeec90fd4d7a264
Bernardo Haab <bernardohaab@gmail.com>
2025-05-22 20:15:33 -0300

    clean project

--- mandelbrot_zoom.c
+++ mandelbrot_zoom.c
@@ -17,120 +17,17 @@
 
 #define NUM_FRAMES 30
 
-const ppm_color_t colors[] = {
-    PPM_BLACK,
-    PPM_BLUE,
-    PPM_GREEN,
-    PPM_CYAN,
-
-    PPM_RED,
-    PPM_MAGENTA,
-    PPM_BROWN,
-    PPM_LIGHT_GREY,
-
-    PPM_DARK_GREY,
-    PPM_BRIGHT_BLUE,
-    PPM_BRIGHT_GREEN,
-    PPM_BRIGHT_CYAN,
-
-    PPM_BRIGHT_RED,
-    PPM_BRIGHT_MAGENTA,
-    PPM_YELLOW,
-    PPM_WHITE};
-const int colornum = sizeof(colors) / sizeof(colors[0]);
-
 void controller(int num_frames);
 void worker();
+
+void generate_zoom_sequence(zoom_coords_t *coords, int num_frames);
+void mandelbrot_render(int *response, double x_min, double x_max, double y_min, double y_max, int width, int height, int frame_id);
+void saveImage(int *colors, int frame_id);
 
 typedef struct
 {
   double x_min, x_max, y_min, y_max;
 } zoom_coords_t;
-
-void generate_zoom_sequence(zoom_coords_t *coords, int num_frames)
-{
-  double x_center = -1.401155;
-  double y_center = 0;
-  double scale = 3; // zoom inicial
-
-  double final_scale = 0.05; // Defina o valor desejado para o zoom final
-  double dynamic_zoom_factor = pow(final_scale / scale, 1.0 / (num_frames - 1));
-  for (int i = 0; i < num_frames; i++)
-  {
-    double width = scale;
-    double height = scale * HEIGHT / WIDTH;
-
-    coords[i].x_min = x_center - width / 2;
-    coords[i].x_max = x_center + width / 2;
-    coords[i].y_min = y_center - height / 2;
-    coords[i].y_max = y_center + height / 2;
-
-    // Ajusta o ZOOM_FACTOR dinamicamente para que o zoom final seja o mesmo independentemente de num_frames
-    scale *= dynamic_zoom_factor;
-  }
-}
-
-void mandelbrot_render(int *response, double x_min, double x_max, double y_min, double y_max, int width, int height, int frame_id)
-{
-  double delta_x = (x_max - x_min) / width;
-  double delta_y = (y_max - y_min) / height;
-
-  for (int row = 0; row < height; row++)
-  {
-    double y0 = y_max - row * delta_y;
-    for (int col = 0; col < width; col++)
-    {
-      double x0 = x_min + col * delta_x;
-      double x = 0, y = 0;
-      int iteration = 0;
-
-      while (x * x + y * y <= MAX_SIZE && iteration < MAX_ITERATIONS)
-      {
-        double xtemp = x * x - y * y + x0;
-        y = 2 * x * y + y0;
-        x = xtemp;
-        iteration++;
-      }
-
-      double norm = (double)iteration / MAX_ITERATIONS;
-      double gamma = 0.4f;
-      double scaled = powf(norm, gamma);
-      int r = (int)(255 * scaled);
-      int g = (int)(255 * powf(scaled, 0.7f));
-      int b = (int)(255 * powf(scaled, 0.5f));
-      int grey = (int)(0.21f * r + 0.72f * g + 0.07f * b);
-
-      response[row * width + col] = grey;
-    }
-  }
-}
-
-int *sliceIntArray(int *source, int from, int to, int *target)
-{
-  // Invalid, return null.
-
-  if (to <= from)
-  {
-    return NULL;
-  }
-
-  // Only allocate if target buffer not given by caller.
-
-  if (target == NULL)
-  {
-    target = malloc((to - from) * sizeof(int));
-    if (target == NULL)
-    {
-      return NULL;
-    }
-  }
-
-  // Copy the data and return it.
-
-  memcpy(target, &(source[from]), (to - from) * sizeof(int));
-
-  return target;
-}
 
 int main(int argc, char *argv[])
 {
@@ -171,29 +68,6 @@ int main(int argc, char *argv[])
 
   MPI_Finalize();
   return EXIT_SUCCESS;
-}
-
-void saveImage(int *colors, int frame_id)
-{
-  char filename[64];
-  snprintf(filename, sizeof(filename), "frame_%03d.ppm", frame_id);
-  // Save image
-  ppm_t *ppm = ppm_create(WIDTH, HEIGHT);
-
-  for (int col = 0; col < WIDTH; col++)
-  {
-    for (int row = 0; row < HEIGHT; row++)
-    {
-      int color = colors[row * WIDTH + col];
-      ppm_color_t grey = {color, color, color};
-      ppm_dot_safe(ppm, col, row, grey);
-    }
-  }
-
-  FILE *f = fopen(filename, "w");
-  ppm_write(ppm, f);
-  fclose(f);
-  ppm_destroy(ppm);
 }
 
 void controller(int num_frames)
@@ -271,4 +145,85 @@ void worker()
     MPI_Send(response, width * height, MPI_INT, 0, status.MPI_TAG, MPI_COMM_WORLD);
     free(response);
   }
+}
+
+void generate_zoom_sequence(zoom_coords_t *coords, int num_frames)
+{
+  double x_center = -1.401155;
+  double y_center = 0;
+  double scale = 3; // zoom inicial
+
+  double final_scale = 0.05; // Defina o valor desejado para o zoom final
+  double dynamic_zoom_factor = pow(final_scale / scale, 1.0 / (num_frames - 1));
+  for (int i = 0; i < num_frames; i++)
+  {
+    double width = scale;
+    double height = scale * HEIGHT / WIDTH;
+
+    coords[i].x_min = x_center - width / 2;
+    coords[i].x_max = x_center + width / 2;
+    coords[i].y_min = y_center - height / 2;
+    coords[i].y_max = y_center + height / 2;
+
+    // Ajusta o ZOOM_FACTOR dinamicamente para que o zoom final seja o mesmo independentemente de num_frames
+    scale *= dynamic_zoom_factor;
+  }
+}
+
+void mandelbrot_render(int *response, double x_min, double x_max, double y_min, double y_max, int width, int height, int frame_id)
+{
+  double delta_x = (x_max - x_min) / width;
+  double delta_y = (y_max - y_min) / height;
+
+  for (int row = 0; row < height; row++)
+  {
+    double y0 = y_max - row * delta_y;
+    for (int col = 0; col < width; col++)
+    {
+      double x0 = x_min + col * delta_x;
+      double x = 0, y = 0;
+      int iteration = 0;
+
+      while (x * x + y * y <= MAX_SIZE && iteration < MAX_ITERATIONS)
+      {
+        double xtemp = x * x - y * y + x0;
+        y = 2 * x * y + y0;
+        x = xtemp;
+        iteration++;
+      }
+
+      double norm = (double)iteration / MAX_ITERATIONS;
+      double gamma = 0.4f;
+      double scaled = powf(norm, gamma);
+      int r = (int)(255 * scaled);
+      int g = (int)(255 * powf(scaled, 0.7f));
+      int b = (int)(255 * powf(scaled, 0.5f));
+      int grey = (int)(0.21f * r + 0.72f * g + 0.07f * b);
+
+      response[row * width + col] = grey;
+    }
+  }
+}
+
+void saveImage(int *colors, int frame_id)
+{
+  char filename[64];
+  snprintf(filename, sizeof(filename), "frame_%03d.ppm", frame_id);
+  // Save image
+  ppm_t *ppm = ppm_create(WIDTH, HEIGHT);
+
+  for (int col = 0; col < WIDTH; col++)
+  {
+    for (int row = 0; row < HEIGHT; row++)
+    {
+      int color = colors[row * WIDTH + col];
+      ppm_color_t grey = {color, color, color};
+      ppm_dot_safe(ppm, col, row, grey);
+    }
+  }
+
+  FILE *f = fopen(filename, "w");
+  ppm_write(ppm, f);
+  fclose(f);
+  ppm_destroy(ppm);
 }
